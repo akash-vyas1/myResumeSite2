095e8f69efe51b9c084912379f589e6cfa3cd528
Akash Vyas <akashvjnv@gmail.com>
2021-05-08 12:24:04 +0530

    certificatePanelDone

--- js/loading.js
+++ js/loading.js
@@ -4,11 +4,16 @@ $(document).ready(function () {
     $('#openHome').click(function () { 
         $('section').css("display","none");
         $('.home').css("display","flex");
-        console.log("Education panel");
+        console.log("Home page");
     });
     $('#openEducation').click(function () { 
         $('section').css("display","none");
         $('.education').css("display","unset");
         console.log("Education panel");
+    });
+    $('#openCertificates').click(function () { 
+        $('section').css("display","none");
+        $('.certificates').css("display","unset");
+        console.log("Certificate panel");
     });
 });
